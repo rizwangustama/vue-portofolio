e1e5234955605e733468d32441b8554f7d21af55
rizwangustama <rizonegustama@gmail.com>
2023-03-05 16:57:48 +0700

    set  up

---
+++ src/views/ContactView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="contact">
+        <h1 class="text-center text-2xl">Selamat Datang di Contact</h1>
+        <h1>Percobaan</h1>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'Contact'
+}
+</script>
+<style lang="scss">
+    .contact{
+        h1{
+            @apply text-blue-500;
+        }
+    }
+</style>
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${path.resolve(__dirname, 'src/styles/variables.scss')}";`,
+      },
+    },
+  },
+})
